babc5bee282af1fffe16716ee13a15df478c58a8
holla394 <holla394@umn.edu>
2023-01-19 18:36:52 -0600

    fixing render website (hopefully)

--- sampleapp/app.py
+++ sampleapp/app.py
@@ -1,33 +1,23 @@
-import numpy as np
+from flask import Flask, render_template
+import pymongo
+import os
 
-
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
-
-# Save reference to the table
-Passenger = Base.classes.test
-
-#################################################
-# Flask Setup
-#################################################
+# Create an instance of our Flask app.
 app = Flask(__name__)
 
+# export 'mongo'='<my mongo password>'
+mongo_password = os.getenv('mongo')
 
-#################################################
-# Flask Routes
-#################################################
+client = pymongo.MongoClient(f"mongodb+srv://sam:{mongo_password}@cluster0.khzagou.mongodb.net/?retryWrites=true&w=majority")
+db = client.natparkapp
+collection = db.twitterData
 
-@app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
 
-if __name__ == '__main__':
-    app.run()
+# Set route
+@app.route('/')
+def index():
+    return collection
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
