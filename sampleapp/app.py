b143da05e5ab8c8953a07b70b9c3efa77d57bf40
holla394 <holla394@umn.edu>
2023-01-19 19:35:30 -0600

    fixed sampleapp

--- sampleapp/app.py
+++ sampleapp/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 import pymongo
 import os
-from config import mongo_password
+# from config import mongo_password
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
 # export 'mongo'='<my mongo password>'
-# mongo_password = os.getenv('mongo')
+mongo_password = os.getenv('mongo')
 
 client = pymongo.MongoClient(f"mongodb+srv://sam:{mongo_password}@cluster0.khzagou.mongodb.net/?retryWrites=true&w=majority")
 db = client.natparkapp
