4b232716ed2267b8da2fb2cc5f0cd93ab6b70ac1
holla394 <holla394@umn.edu>
2023-01-23 21:28:29 -0600

    got selection menu working and updated app.py with api paths

--- sampleapp/app.py
+++ sampleapp/app.py
@@ -1,11 +1,11 @@
-import flask
+from flask import Flask, render_template, jsonify
 import pymongo
 import logging
 import os
 # from config import mongo_password
 
 # Create an instance of our Flask app.
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 # export 'mongo'='mongo1'
 mongo_password = os.getenv('mongo')
@@ -13,6 +13,7 @@ mongo_password = os.getenv('mongo')
 client = pymongo.MongoClient(f"mongodb+srv://sam:{mongo_password}@cluster0.khzagou.mongodb.net/?retryWrites=true&w=majority")
 db = client.natparkapp
 twitterdata = db.twitterData
+geojson = db.geojson
 
 # logging.log(msg='###################################################')
 # logging.log(msg='###################################################')
@@ -29,7 +30,27 @@ def index():
     return_list = []
     for result in results:
         return_list.append(result)
-    return flask.render_template("index.html", return_list=return_list)
+    return render_template("index.html", return_list=return_list)
+
+
+# Set route
+@app.route('/api/v1/<parkname>')
+def park(parkname):
+    results = geojson.find_one({'parkname':parkname})
+    del results['_id']
+    new_dict = {
+        'type':'FeatureCollection',
+        'features':[results]
+    }
+    return new_dict
+
+# get list of park names
+@app.route('/api/v1/allparknames')
+def parknames():
+    results = geojson.find()
+    parks_list = [result['parkname'] for result in results if result['parkname']]
+    return jsonify(parks_list)
 
 if __name__ == "__main__":
+    # change True to False when ready for deployment
     app.run(debug=True)
--- sampleapp/static/js/logic.js
+++ sampleapp/static/js/logic.js
@@ -4,35 +4,64 @@ let myMap = L.map("map", {
   zoom: 6
 });
 
+let base_url = window.location.href;
+
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 // Use this link to get the GeoJSON data.
-const link = "https://nationalparksgeojson01192023.s3.us-east-2.amazonaws.com/NPS_-_Land_Resources_Division_Boundary_and_Tract_Data_Service.geojson";
+// const link = "https://nationalparksgeojson01192023.s3.us-east-2.amazonaws.com/NPS_-_Land_Resources_Division_Boundary_and_Tract_Data_Service.geojson";
 
 // Getting our GeoJSON data
-const successCb = (resp) => {
-    console.log(resp);
-	L.geoJSON(resp).addTo(myMap);
+// const successCb = (resp) => {
+//     console.log(resp);
+// 	L.geoJSON(resp).addTo(myMap);
+// };
+
+// const errorCb = (err) => {
+//     console.error('Error - ', err);
+// };
+
+function optionChanged(selection) {
+  displayborder(selection);
+  // refreshTwitter(selection);
+  // refreshArticles(selection);
 };
 
-const errorCb = (err) => {
-    console.error('Error - ', err);
+function displayborder(parkname) {
+  let geojson_api_url = `${base_url}/api/v1/${parkname}`
+  d3.json(geojson_api_url).then(data => {
+    L.geoJson(data.features[0].geometry.coordinates).addTo(myMap)
+  })
 };
 
-function downloadObject(link, successCb, errorCb) {
-    fetch(link)
-      .then(response => response.json())
-      .then(successCb)
-      .catch(errorCb);
-}
+names = `${base_url}/api/v1/allparknames`;
 
-downloadObject(link, successCb, errorCb);
+let parkname = "Yellowstone National Park";
 
+function loadpage(parkname) {
+  let parknames_url = `${base_url}/api/v1/allparknames`;
+  d3.json(parknames_url).then(names => {
+    names.forEach( (name) =>
+        {
+          let drop_down = d3.select("#selDataset");
+          drop_down.append("option").text(name);
+        });
+  });
+  displayborder(parkname);
+  // refreshTwitter(selection);
+  // refreshArticles(selection);
+};
 
-d3.json(link).then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
-});
+// function downloadObject(link, successCb, errorCb) {
+//     fetch(link)
+//       .then(response => response.json())
+//       .then(successCb)
+//       .catch(errorCb);
+// };
+
+// downloadObject(link, successCb, errorCb);
+
+loadpage(parkname);
